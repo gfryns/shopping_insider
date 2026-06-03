9ced04846802e114e6d743331fec76a060342999
Gregory Fryns <gfryns@google.com>
2026-06-03 17:10:22 +0100

    refactor: update product views and historical aggregations to use unique_product_id instead of unique_offer_id

--- sql/1_product_view.sql
+++ sql/1_product_view.sql
@@ -148,10 +148,8 @@ AS (
         Products.product_type,
         Products.additional_product_types,
         IF(ApprovedOffer.product_id IS NULL, 0, 1) AS is_approved,
-        CONCAT(CAST(Products.merchant_id AS STRING), '|', Products.product_id)
-          AS unique_product_id,
         CONCAT(CAST(Products.merchant_id AS STRING), '|', Products.offer_id)
-          AS unique_offer_id,
+          AS unique_product_id,
         IFNULL(SPLIT(Products.product_type, '>')[SAFE_OFFSET(0)], 'N/A') AS product_type_l1,
         IFNULL(SPLIT(Products.product_type, '>')[SAFE_OFFSET(1)], 'N/A') AS product_type_l2,
         IFNULL(SPLIT(Products.product_type, '>')[SAFE_OFFSET(2)], 'N/A') AS product_type_l3,
